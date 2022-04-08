31af08be2d19fd591fc35851063b5ebe261a4ca0
bumsu <hotsem8@gmail.com>
2022-04-08 18:05:35 +0900

    07 최종

--- finally/src/kunsan/bumsu/lecture05/DevideByZeroHandling.java
+++ finally/src/kunsan/bumsu/lecture05/DevideByZeroHandling.java
@@ -10,12 +10,15 @@ public class DevideByZeroHandling {
         int check = 0;
         int dividend = 0;
         int divisor = 0;
-        Scanner scanner = new Scanner(System.in);
+        
 
         while (check == 0) {
         	try {
-        		System.out.print("나뉨수를 입력하시오 : ");
+        		
+         		System.out.print("나뉨수를 입력하시오 : ");
+         		Scanner scanner = new Scanner(System.in);
                 dividend=scanner.nextInt();    // 나뉨수 입력
+               //String test= scanner.nextLine();
                 if (dividend == 1234) {
                     System.out.println("프로그램을 종료합니다.");
                     check = 1;
@@ -26,19 +29,21 @@ public class DevideByZeroHandling {
         	}
         	catch(InputMismatchException e) {
             	System.out.println("정수를 입력하시오.");
-            	System.out.println("00");
-            	break;
+            	continue;
+            	
             }
+        	
             try {
                 System.out.println(dividend + "를 "+ divisor+"로 나누면 몫은 " + dividend/divisor + "입니다.");
                 break;    // 정상적인 나누기 완료 후 while 벗어나기
             }
             catch(ArithmeticException e) {   // ArithmeticException 예외 처리 코드
-                System.out.println("0으로 나눌 수 없습니다! \n 다시 입력하세요.");
+            	if (divisor == 0)
+            		System.out.println("0으로 나눌 수 없습니다! \n 다시 입력하세요.");
             }
             
         }
-    }// 여기까지, git test
+    }
 
     public static void main(String[] args) {
         Main aMan = new Main();
