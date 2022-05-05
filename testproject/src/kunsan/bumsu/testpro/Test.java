2cdbc0020523b9cbdc26340d864caef8d43e3ac7
hotsem4 <hotsem8@gmail.com>
2022-05-05 09:10:55 +0900

    test용 project

---
+++ testproject/src/kunsan/bumsu/testpro/Test.java
@@ -0,0 +1,24 @@
+package kunsan.bumsu.testpro;
+
+import java.util.Scanner;
+
+public class Test {
+	public void testArray() {
+		int a = 0;
+		int b = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("a의 값을 입력해주세요.");
+		a = sc.nextInt();
+		System.out.print("b의 값을 입력해주세요.");
+		b = sc.nextInt();
+		
+		int test = a + b;
+		
+		System.out.println("a + b = " + test);
+		
+		
+		
+		
+	}
+}
